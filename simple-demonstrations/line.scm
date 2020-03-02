49ac43ff0f4f5f18275447c9746683e1f31d8349
René Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2020-03-02 19:53:58 +0100

    paintbrush vs pencil

--- simple-demonstrations/line.scm
+++ simple-demonstrations/line.scm
@@ -9,20 +9,30 @@
    (gimp-context-set-background '(000 000 255))
    (gimp-context-set-foreground '(255 000 000))
 
-   (gimp-drawable-fill lyr FILL-BACKGROUND)
+   (gimp-drawable-fill          lyr FILL-BACKGROUND)
    (gimp-image-insert-layer img lyr 0 0)
+ ;
+ ;  Assign coordinates for first line to be drawn:
+ ;
+   (vector-set! coords 0  20.0) ; x1
+   (vector-set! coords 1  20.0) ; y1
+   (vector-set! coords 2 230.0) ; x2
+   (vector-set! coords 3 230.0) ; y2
+
+   (gimp-context-set-brush            brs   )
+   (gimp-context-set-brush-size       19.0  )
+   (gimp-context-set-brush-hardness    0.5  )
+;  (gimp-context-set-brush-spacing    0.01  ) ; Is this sometimes necessary?
+
+   (gimp-pencil lyr 4 coords)
 
  ;
- ;  Assign coordinates for line to be drawn:
+ ;  Assign coordinates for second line to be drawn:
  ;
-   (vector-set! coords 0  10.0) ; x1
-   (vector-set! coords 1  10.0) ; y1
-   (vector-set! coords 2 240.0) ; x2
-   (vector-set! coords 3 240.0) ; y2
-
-   (gimp-context-set-brush            brs  )
-   (gimp-context-set-brush-size       4.0  )
-   (gimp-context-set-brush-spacing    0.01 ) ; Try to make a smooth line
+   (vector-set! coords 0 230.0) ; x1
+   (vector-set! coords 1  20.0) ; y1
+   (vector-set! coords 2  20.0) ; x2
+   (vector-set! coords 3 230.0) ; y2
 
    (gimp-paintbrush lyr
          0              ; fade-out
