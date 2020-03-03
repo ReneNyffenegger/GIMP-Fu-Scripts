88a504802e753eebd1ea939f2af68906b4defd5e
René Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2020-03-03 08:59:41 +0100

    Opacity = 100

--- simple-demonstrations/brush-spacing.scm
+++ simple-demonstrations/brush-spacing.scm
@@ -1,7 +1,7 @@
 (let* (
-     (my_image   (car (gimp-image-new 250 100 RGB      )))
-     (my_layer   (car (gimp-layer-new my_image 250 100 RGB-IMAGE "my layer" 0 LAYER-MODE-NORMAL-LEGACY)))
-     (tq84-brush (car (gimp-brush-new "TQ84 Brush")))
+     (img (car (gimp-image-new     250 100 RGB                                            )))
+     (lyr (car (gimp-layer-new img 250 100 RGB-IMAGE "my layer" 0 LAYER-MODE-NORMAL-LEGACY)))
+     (brs (car (gimp-brush-new                       "TQ84 Brush"                         )))
 
      (lin-1 (make-vector 4))
      (lin-2 (make-vector 4))
@@ -35,34 +35,35 @@
    (vector-set! lin-5 2 225) ; x2
    (vector-set! lin-5 3  90) ; y2
 
-   (gimp-image-add-layer my_image my_layer 0)
 
    (gimp-context-set-background '(240 220 230))
    (gimp-context-set-foreground '(255 100  30))
 
-   (gimp-drawable-fill my_layer FILL-BACKGROUND)
+   (gimp-drawable-fill lyr FILL-BACKGROUND)
 
-   (gimp-brush-set-shape           tq84-brush BRUSH-GENERATED-CIRCLE)
-   (gimp-context-set-brush         tq84-brush)
+   (gimp-image-insert-layer img lyr 0 0)
+
+   (gimp-brush-set-shape           brs BRUSH-GENERATED-CIRCLE)
+   (gimp-context-set-brush         brs)
    (gimp-context-set-brush-size       9.0 )
    (gimp-context-set-brush-spacing    3.5 )
 
-   (gimp-paintbrush my_layer 0 4 lin-1 PAINT-CONSTANT 0)
+   (gimp-paintbrush lyr 0 4 lin-1 PAINT-CONSTANT 0)
 
    (gimp-context-set-brush-spacing    2.0 )
-   (gimp-paintbrush my_layer 0 4 lin-2 PAINT-CONSTANT 0)
+   (gimp-paintbrush lyr 0 4 lin-2 PAINT-CONSTANT 0)
 
    (gimp-context-set-brush-spacing    1.5 )
-   (gimp-paintbrush my_layer 0 4 lin-3 PAINT-CONSTANT 0)
+   (gimp-paintbrush lyr 0 4 lin-3 PAINT-CONSTANT 0)
 
    (gimp-context-set-brush-spacing    8.0 )
-   (gimp-paintbrush my_layer 0 4 lin-4 PAINT-CONSTANT 0)
+   (gimp-paintbrush lyr 0 4 lin-4 PAINT-CONSTANT 0)
 
    (gimp-context-set-brush-spacing    1.0 )
-   (gimp-paintbrush my_layer 0 4 lin-5 PAINT-CONSTANT 0)
+   (gimp-paintbrush lyr 0 4 lin-5 PAINT-CONSTANT 0)
 
-   (gimp-file-save RUN-NONINTERACTIVE my_image my_layer "f:\\img\\brush-spacing.jpg" "")
-   (gimp-brush-delete tq84-brush)
-   (gimp-image-delete my_image)
+   (gimp-file-save RUN-NONINTERACTIVE img lyr "f:\\img\\brush-spacing.jpg" "")
+   (gimp-brush-delete brs)
+   (gimp-image-delete img)
 )
 (gimp-quit TRUE)
--- simple-demonstrations/text.scm
+++ simple-demonstrations/text.scm
@@ -1,13 +1,13 @@
 (let* (
-        (img (car (gimp-image-new          250 100 RGB                                              )))
+        (img (car (gimp-image-new     250 100 RGB                                              )))
         (lyr (car (gimp-layer-new img 250 100 RGB-IMAGE "my layer" 100 LAYER-MODE-NORMAL-LEGACY)))
       )
-
-   (gimp-image-insert-layer img lyr 0 0)
 
    (gimp-context-set-background '(  0 063 127))
    (gimp-context-set-foreground '( 63 127   0))
    (gimp-drawable-fill lyr FILL-BACKGROUND)
+
+   (gimp-image-insert-layer img lyr 0 0)
 
    (gimp-floating-sel-anchor (car ; Anchor specified floating selection to its associated drawable
       (gimp-text-fontname
