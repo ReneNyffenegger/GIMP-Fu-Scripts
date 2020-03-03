23060ac3c820bb21d98c5ea04a383e9393b3645f
René Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2020-03-03 08:46:58 +0100

    Use opacity = 100 etc.

--- simple-demonstrations/circle.scm
+++ simple-demonstrations/circle.scm
@@ -1,6 +1,6 @@
 (let* (
-     (img (car (gimp-image-new     250 250 RGB                                            )))
-     (lyr (car (gimp-layer-new img 250 250 RGB-IMAGE "my layer" 0 LAYER-MODE-NORMAL-LEGACY)))
+     (img (car (gimp-image-new     250 250 RGB                                              )))
+     (lyr (car (gimp-layer-new img 250 250 RGB-IMAGE "my layer" 100 LAYER-MODE-NORMAL-LEGACY)))
    )
 
    (gimp-context-set-background '(250 230 240))
@@ -8,7 +8,7 @@
 
    (gimp-drawable-fill lyr FILL-BACKGROUND)
 
-   (gimp-image-add-layer img lyr 0)
+   (gimp-image-insert-layer img lyr 0 0)
 
 ;  (gimp-context-set-antialias TRUE)
 
@@ -16,12 +16,13 @@
           img                ;
           CHANNEL-OP-REPLACE ; operation (CHANNEL-OP-{ADD,SUBTRACT,REPLACE,INTERSECXT} )
            25  25            ; x and y coordinate of the upper-left  corner of the ellipse bounding box
-          200 200            ; width and heigth of the ellipse
+          200 200            ; width and height of the ellipse
    )
 ;  (gimp-edit-stroke lyr)
    (gimp-drawable-edit-stroke-selection lyr)
 
-   (gimp-file-save RUN-NONINTERACTIVE img lyr "f:\\img\\circle.jpg" "")
+   (gimp-file-save RUN-NONINTERACTIVE img lyr "f:\\img\\circle.jpg" ""      )
+;  (gimp-xcf-save RUN-NONINTERACTIVE  img lyr "f:\\img\\circle.xcf" "circle")
 
    (gimp-image-delete img)
 )
