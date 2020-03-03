cfee27ab36b998e04e33885ff08405a8921788cf
René Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2020-03-03 08:53:22 +0100

    Opacity = 100

--- simple-demonstrations/filled-circle.scm
+++ simple-demonstrations/filled-circle.scm
@@ -1,25 +1,28 @@
 (let* (
-     (my-image (car (gimp-image-new          250 250 RGB                                            )))
-     (my-layer (car (gimp-layer-new my-image 250 250 RGB-IMAGE "my layer" 0 LAYER-MODE-NORMAL-LEGACY)))
+     (img (car (gimp-image-new     250 250 RGB                                              )))
+     (lyr (car (gimp-layer-new img 250 250 RGB-IMAGE "my layer" 100 LAYER-MODE-NORMAL-LEGACY)))
    )
-
-   (gimp-image-add-layer my-image my-layer 0)
 
    (gimp-context-set-background '( 10  40  30))
    (gimp-context-set-foreground '(255 240 230))
 
-   (gimp-drawable-fill my-layer FILL-BACKGROUND )
+   (gimp-drawable-fill lyr FILL-BACKGROUND)
+
+;  (gimp-image-add-layer img lyr 100)
+   (gimp-image-insert-layer img lyr 0 0)
 
    (gimp-image-select-ellipse
-          my-image           ;
+          img                ;
           CHANNEL-OP-REPLACE ; operation (CHANNEL-OP-{ADD,SUBTRACT,REPLACE,INTERSECXT} )
            25  25            ; x and y coordinate of the upper-left  corner of the ellipse bounding box
-          200 200            ; widht and height of the ellipse
+          200 200            ; width and height of the ellipse
    )
 
-   (gimp-drawable-edit-fill my-layer FILL-FOREGROUND)
+   (gimp-drawable-edit-fill lyr FILL-FOREGROUND)
 
-   (gimp-file-save RUN-NONINTERACTIVE my-image my-layer "f:\\img\\filled-circle.jpg" "")
-   (gimp-image-delete my-image)
+   (gimp-file-save RUN-NONINTERACTIVE img lyr "f:\\img\\filled-circle.jpg" ""             )
+;  (gimp-xcf-save RUN-NONINTERACTIVE  img lyr "f:\\img\\filled-circle.xcf" "filled-circle")
+
+   (gimp-image-delete img)
 )
 (gimp-quit TRUE)
